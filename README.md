6a2979d8d72db4ba9d06edc8ca82e8f802a6d2a7
Luis Crespo <lcrespo@es.ibm.com>
2017-01-19 11:51:29 +0100

    Uglify and DEV/PROD environments

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # webpack2-ts-template
 Template for building apps with WebPack 2 and TypeScript
 
-## Cheat sheet
+## Notes
 - `npm view webpack dist-tags` => see version tags for webpack (or any other package)
+- It's 2017, and UglifyJS does not support ES6
 
 ## ToDo
-- Tree shaking
-- Uglify
+- Tree shaking => check if already working after enabling uglify
 - Importing from external lib without packaging (e.g. Ramda)
 	- See 'externals' webpack config entry
-- Environments: DEV / PROD
-	- Use in config
-	- Use in runtime
+- Access config options in runtime (e.g. DEV/PROD environment)
 
 ## Done
 - TypeScript support
 - Source maps
 - Dev server
+- Uglify
+	- Only for production environment, without source maps
+	- Source maps could be enabled for production with `{ sourceMap: true }`
+		as plugin parameter.
+- Environments: DEV / PROD
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,52 @@
 var path = require('path');
+var webpack = require('webpack');
 
-module.exports = {
-	devtool: 'source-map',
-	entry: {
-		app1: ['./src/app1/main.ts'],
-		app2: ['./src/app2/main.ts']
-	},
-	resolve: {
-		extensions: ['.webpack.js', '.ts', '.js']
-	},
-	module: {
-		loaders: [{
-	        test: /\.ts$/,
-			loader: 'ts-loader',
-			exclude: '/node_modules'
-		}]
-	},
-	output: {
-		path: path.resolve(__dirname, 'web'),
-		filename: '[name].bundle.js'
+const PROD = 1, DEV = 2;
+
+function getTarget(env) {
+	if (!env.target) {
+		console.error('ERROR: no "target" environment found!');
+		console.error('       use --env.target=PROD|DEV in webpack command line');
+		process.exit(2);
 	}
-};
+	console.log('Building for target: ' + env.target);
+	switch (env.target) {
+		case 'PROD': return PROD;
+		case 'DEV': return DEV;
+		default:
+			console.error(`ERROR: invalid target "${env.target}"`);
+			process.exit(1);
+	}
+}
+
+module.exports = function(env) {
+	let target = getTarget(env);
+	let plugins = [];
+	if (target == PROD) {
+		plugins = [
+			new webpack.optimize.UglifyJsPlugin()
+		];
+	}
+	return {
+		devtool: 'source-map',
+		entry: {
+			app1: ['./src/app1/main.ts'],
+			app2: ['./src/app2/main.ts']
+		},
+		resolve: {
+			extensions: ['.webpack.js', '.ts', '.js']
+		},
+		module: {
+			loaders: [{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: '/node_modules'
+			}]
+		},
+		plugins,
+		output: {
+			path: path.resolve(__dirname, 'web'),
+			filename: '[name].bundle.js'
+		}
+	};
+}
